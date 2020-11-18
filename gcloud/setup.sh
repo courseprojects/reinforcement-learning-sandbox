7e81fc9fb805d678be51bf641976dabc12ec9882
Peter David Fagan <peterdavidfagan@gmail.com>
2020-11-18 00:05:53 +0000

    sudo

--- gcloud/setup.sh
+++ gcloud/setup.sh
@@ -2,5 +2,5 @@ sudo git clone https://github.com/peterdavidfagan/CS221-Project.git
 sudo chmod -R a+rwx ./CS221-Project
 cd ./CS221-Project/docker/
 sudo gsutil cp -r gs://robot-license/mjkey.txt ./
-sudo sudo docker build -t robosuite .
+sudo docker build -t robosuite .
 sudo docker run -it -d --gpus all --name robosuite robosuite:latest
