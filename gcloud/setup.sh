fbb35c8afb2d85fd95d6726d5306ccfe339e8373
Peter David Fagan <peterdavidfagan@gmail.com>
2020-11-18 21:50:55 +0000

    adding setup and sweep update

--- gcloud/setup.sh
+++ gcloud/setup.sh
@@ -1,3 +1,16 @@
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+deb [arch=amd64] https://download.docker.com/linux/debian buster stable
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+sudo apt -y install git
+
+
 sudo git clone https://github.com/peterdavidfagan/CS221-Project.git
 sudo chmod -R a+rwx ./CS221-Project
 cd ./CS221-Project/docker/
