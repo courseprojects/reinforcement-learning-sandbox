3a8c266b6bd6e6ee7af31692cf3224d226b724d6
Peter David Fagan <peterdavidfagan@gmail.com>
2020-11-21 01:04:34 +0000

    adding evaluation function

--- test.py
+++ test.py
@@ -56,13 +56,20 @@ def create_media():
     frames = []
     trajectory = []
     cube_pos = []
+    grip=False
+    grip_success = 0
     for i in range(args.horizon):
         if args.algo=='REINFORCE':
             action, log_prob = agent.select_action(state)
         else:
-            action = agent.select_action(state)  
-        cube_pos.append(env.sim.data.body_xpos[env.cube_body_id])
-        trajectory.append(list(env.sim.data.site_xpos[env.robots[0].eef_site_id]))
+            action = agent.select_action(state)
+
+        grip = check_grip(env)
+        if (grip==False) & (grip_success == 0):  
+            cube_pos.append(env.sim.data.body_xpos[env.cube_body_id])
+            trajectory.append(list(env.sim.data.site_xpos[env.robots[0].eef_site_id]))
+        else:
+            grip_success = 1
         obs, reward, done, info = env.step(action)
         state = np.append(obs['robot0_robot-state'],obs['object-state'])
         # dump a frame from every K frames
@@ -79,13 +86,14 @@ def create_media():
     fig.add_trace(go.Scatter3d(x=[x[0] for x in trajectory],y=[x[1] for x in trajectory],z=[x[2] for x in trajectory]))
     fig.add_trace(go.Scatter3d(x=[cube_pos[0][0]],y=[cube_pos[0][1]],z=[cube_pos[0][2]],
                                 marker=dict(
-                                size=20,
+                                size=30,
                                 symbol='square',
                             )))
     fig.update_layout()
     fig.show()
     fig.write_image('plot.png')
     writer.close()
+
 
 def watch_trajectory():
     # Setting up the robot enviornment
@@ -116,18 +124,78 @@ def watch_trajectory():
         env.render()
 
 
+def check_grip(env):
+    touch_left_finger = False
+    touch_right_finger = False
+    for i in range(env.sim.data.ncon):
+        c = env.sim.data.contact[i]
+        if c.geom1 in env.l_finger_geom_ids and c.geom2 == env.cube_geom_id:
+            touch_left_finger = True
+        if c.geom1 == env.cube_geom_id and c.geom2 in env.l_finger_geom_ids:
+            touch_left_finger = True
+        if c.geom1 in env.r_finger_geom_ids and c.geom2 == env.cube_geom_id:
+            touch_right_finger = True
+        if c.geom1 == env.cube_geom_id and c.geom2 in env.r_finger_geom_ids:
+            touch_right_finger = True
+    if touch_left_finger and touch_right_finger:
+        return True
+    else:
+        return False
+
+
+def evaluate_grip_goal():
+    env = suite.make(
+        env_name=args.env_name,
+        robots=args.robot,
+        has_renderer=False,
+        has_offscreen_renderer=False,
+        use_camera_obs=False,
+        use_object_obs=True,                    
+        horizon = args.horizon, 
+        reward_shaping=False                
+    )
+    success_count = 0 
+    for test in range(args.num_trials):
+        obs = env.reset()
+        state_dim = obs['robot0_robot-state'].shape[0]+obs['object-state'].shape[0]
+        state = np.append(obs['robot0_robot-state'],obs['object-state'])
+        agent = set_agent(state_dim, env, args)
+
+        # Visualize a single run
+        done=False
+        while done==False: 
+            if args.algo=='REINFORCE':
+                action, log_prob = agent.select_action(state)
+            else:
+                action = agent.select_action(state)           
+            obs, reward, done, info = env.step(action)
+            if check_grip(env)==True:
+                success_count+=1
+                print('The robot succeded in gripping the block')
+                break
+
+            state = np.append(obs['robot0_robot-state'],obs['object-state'])
+        if done==True:
+            print('The robot failed to grip the block')
+
+    print(success_count/args.num_trials)
+
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='PyTorch robot arm testing script')
     parser = argparse.ArgumentParser(description='PyTorch robot arm training script')
+    parser.add_argument('--test_cat', type=str, default='watch')
     parser.add_argument('--env_name', type=str, default='Lift')
     parser.add_argument('--robot', type=str, default='Panda')
     parser.add_argument('--algo', type=str, default='DDPG')
     parser.add_argument('--model_path', type=str, default='Lift')
     parser.add_argument('--render', type=bool, default=True)
-    parser.add_argument("--camera", type=str, default="frontview", help="Name of camera to render")
-    parser.add_argument("--height", type=int, default=512)
-    parser.add_argument("--width", type=int, default=512)
-    parser.add_argument("--video_path", type=str, default="video.mp4")
+    parser.add_argument('--camera', type=str, default='frontview', help='Name of camera to render')
+    parser.add_argument('--height', type=int, default=512)
+    parser.add_argument('--width', type=int, default=512)
+    parser.add_argument('--video_path', type=str, default='video.mp4')
+    parser.add_argument('--num_trials', type=int, default=25)
     parser.add_argument("--skip_frame", type=int, default=1)
     parser.add_argument('--enable_her',type=bool,default=False)
     parser.add_argument('--dense_rewards',type=bool,default=True)
@@ -148,5 +216,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #watch_trajectory()
-    create_media()
+    if args.test_cat=='watch':
+        watch_trajectory()
+    elif args.test_cat=='output_media':
+        create_media()
+    elif args.test_cat=='evaluate_grip':
+        evaluate_grip_goal()
+    else:
+        sys.exit('Incorrect test specification.')
+
+
