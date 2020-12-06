9bfaf9cfdc7b9fce7e884a896e01244cc2649952
Peter David Fagan <peterdavidfagan@gmail.com>
2020-12-06 16:46:39 +0000

    setup updates

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='roboray',
     version='0.1.0',    
     description='Package for running robosuite with ray',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/peterdavidfagan/CS221-Project',
     author='Peter David Fagan',
     author_email='pfagan@stanford.edu',
