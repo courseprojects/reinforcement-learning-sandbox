61f2139b14bebbd454dc1cdeb67b667636501e06
Peter David Fagan <peterdavidfagan@gmail.com>
2020-12-06 16:29:27 +0000

    adding to setup

--- setup.py
+++ setup.py
@@ -4,11 +4,24 @@ setup(
     name='roboray',
     version='0.1.0',    
     description='Package for running robosuite with ray',
+    url='https://github.com/peterdavidfagan/CS221-Project'
     author='Peter David Fagan',
     author_email='pfagan@stanford.edu',
     license='MIT',
     packages=['roboray'],
     python_requires='>=3.5',
+    install_requires=[
+    	glfw>=1.4.0
+		numpy>=1.13.3
+		Cython>=0.27.2
+		imageio>=2.1.2
+		cffi>=1.10
+		fasteners~=0.15
+		jupyter
+		torch
+		robosuite
+		wandb
+    ]
     entry_points = {
     	'console_scripts':['roboray-train=roboray.cli:main']
     }
