ce50cf5434ed5191b2ac935e410c94b2911ef30a
Peter David Fagan <peterdavidfagan@gmail.com>
2021-01-02 18:13:07 +0000

    updating package name

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='roboray',
-    version='0.3.0',    
-    description='Package for testing rl algorithms',
+    name='peters_rl_sandbox',
+    version='0.1.0',    
+    description='Package for testing reinforcement learning algorithms',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/peterdavidfagan/Roboray',
+    url='https://github.com/peterdavidfagan/reinforcement_learning_sandbox.git',
     author='Peter David Fagan',
     author_email='peterdavidfagan@gmail.com',
     license='MIT',
-    packages=['roboray'],
     python_requires='>=3.5',
     install_requires=[
     	'glfw>=1.4.0',
@@ -31,6 +30,6 @@ setup(
         'pyyaml'
     ],
     entry_points = {
-    	'console_scripts':['roboray-train=roboray.cli.cli_temp:main']
+    	'console_scripts':['sandbox=sandbox.cli.cli_temp:main']
     }
 )
